7ff8c34f75d4e864104cf83e496cb794f5d8976c
Mariyam-Asif <mariyamasif530@gmail.com>
2024-08-21 17:29:14 +0500

    initial commit

---
+++ data/index.ts
@@ -0,0 +1,149 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Services", link: "#services" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I emphasize client collaboration, ensuring open and effective communication.",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly strive to enhance ",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast driven by passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "I blend creativity with technical expertise, turning your vision into standout solutions that make a real impact.",
+      description: "The Inside Track",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Let’s build something extraordinary together!",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "CLI Currency Converter",
+      des: "Effortlessly convert global currencies with this CLI Currency Converter, using Inquirer.",
+      img: "/currency-converter.png",
+      iconLists: ["/ts.svg","/npm.svg"],
+      link: "https://github.com/Mariyam-Asif/currency-converter",
+    },
+    {
+      id: 2,
+      title: "To-Do List Application",
+      des: "Simplify task management with this CLI To-Do List. Effortlessly add, view, and organize your tasks with ease.",
+      img: "/to-do.png",
+      iconLists: ["/ts.svg","/npm.svg"],
+      link: "https://github.com/Mariyam-Asif/todo-list-inquirer",
+    },
+    {
+      id: 3,
+      title: "Number Guessing Game",
+      des: "Engage in a fun CLI-based Number Guessing Game, built with TypeScript and Inquirer.",
+      img: "/number-guessing-game.png",
+      iconLists: ["/ts.svg", "/npm.svg"],
+      link: "https://github.com/Mariyam-Asif/cli-number-guessing",
+    },
+    {
+      id: 4,
+      title: "Digital Clock",
+      des: "Track time precisely with this Digital Clock, using JavaScript, HTML, and CSS.",
+      img: "/clock.png" ,
+      iconLists: ["/css-3.svg", "/ts.svg", "/javascript.svg","/html.svg"],
+      link: "https://github.com/Mariyam-Asif/digital-clock",
+    },
+  ];
+  
+  export const services = [
+    {
+      id: 1,
+      title: "Innovation at Your Fingertips",
+      desc: " Bringing cutting-edge tech to your projects, making sure you’re ahead of the curve.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Tailored Solutions",
+      desc: "Every project is unique - Expect nothing less than personalized, high-impact results.",
+      className: "md:col-span-2", 
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Quality That Speaks",
+      desc: "My code isn’t just functional; it’s a work of art—efficient, elegant, and built to last.",
+      className: "md:col-span-2", 
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Client-Focused Creativity",
+      desc: " I don’t just work for you; I work with you, turning your vision into a reality that wows.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      url: "https://github.com/Mariyam-Asif",
+    },
+    {
+      id: 2,
+      img: "/link.svg",
+      url: "https://www.linkedin.com/in/mariyam-asif-4b5b252b5/",
+    },
+  ];
